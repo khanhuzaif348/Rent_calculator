6657dc47ecf45c5ff50daf960d42690a13e7b8e0
Mohd HUzaif <103990272+khanhuzaif348@users.noreply.github.com>
2025-01-09 18:50:01 +0530

    Create rent_cal.py

---
+++ rent_cal.py
@@ -0,0 +1,21 @@
+##Input we need from the user 
+
+#Total rent 
+#Total food ordered for snacking 
+#Electricity units spend
+#Charge per unit 
+# Input   members 
+## output
+ #total amount you're to pay is  
+
+rent = int(input("Enter your flat rent: - "))
+food = int(input("Enter food has ordered : - "))
+electric_spends = int(input("Enter electricity charge spend : - "))
+charge_per_unit = int(input("Enter charge : -  "))
+person = int(input("Enter the number of person living in room  : - "))
+total_bill = electric_spends * charge_per_unit 
+
+total_amount = rent + food + total_bill
+print(f'Your total rent : -  {total_amount}')
+amount_each_person = total_amount // person
+print(f'you are a  {person}  member  and each person rent  : - {amount_each_person}')
